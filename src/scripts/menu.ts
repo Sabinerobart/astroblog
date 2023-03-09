cc964ebc21b92be06ab939b91d649da1f547e073
Sabine Robart <sabine.robart@protonmail.com>
2023-03-09 16:26:01 +0100

    feat: handle dark theme (interactions with astro alone - no UI framework)

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,5 +1,7 @@
-(function () {
+export const menu = () => {
   document.querySelector(".hamburger").addEventListener("click", () => {
     document.querySelector(".nav-links").classList.toggle("expanded");
   });
-})();
+};
+
+menu();
